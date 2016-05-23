6f659474b669916988e9b027c7532bb20d77de92
fstahlberg <fstahlberg@gmail.com>
2016-05-23 18:00:32 +0100

    * fix pythonpath in train.py

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
 """Redirect to cam.sgnmt.blocks.train """
 
 import sys
+import os
+import inspect
 
-sys.path.insert(0, './cam/sgnmt/blocks')
+sys.path.insert(0, "%s/cam/sgnmt/blocks"
+   % os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 
 import cam.sgnmt.blocks.train
